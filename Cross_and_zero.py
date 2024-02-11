e44e2ba5f0949815bdeb5caee5ce21f6360fbac7
grib <nikita-gribkov@inbox.ru>
2024-02-12 01:15:45 +0300

    Ready 1

--- Cross_and_zero.py
+++ Cross_and_zero.py
@@ -1,14 +1,3 @@
-board = [1,'|',2,'|',3,
-        4,'|',5,'|',6,
-        7,'|',8,'|',9]
-line_to_victory = [[0, 2, 4],
-             [5, 7, 9],
-             [10, 12, 14],
-             [0, 7, 14],
-             [4, 7, 10],
-             [0, 5, 10],
-             [2, 7, 12],
-             [4, 9, 14]]
 
 def show_board():
     print('\n-----------')
@@ -42,8 +31,6 @@ def get_result():
             win = 1
         if board[i[0]] == "o" and board[i[1]] == "o" and board[i[2]] == "o":
             win = 2
-    if count == 9:
-        win = 3
     return win
 
 def start_game():
@@ -53,31 +40,32 @@ def start_game():
 
 def winner(win):
     if win == 1:
-        print(f'победил {player1}')
+        wins[0] = wins[0] + 1
+        print(f'победил {player1}  {wins[0]} раз(а)')
+        show_board()
     if win == 2:
-        print(f'победил {player2}')
+        wins[1] = wins[1] + 1
+        print(f'победил {player2} {wins[1]} раз(а)')
+        show_board()
     if win == 3:
         print('Ничья')
-
-def play():
-    while one_more() == True:
-        start_game()
-        win = game()
-        winner(win)
-    print("спасибо за игру")
+        show_board()
 
 def one_more():
+    global board
     flag1 = True
     print("хотите сыграть?  y/n - ")
     play = input()
     if play == 'y':
         flag1 = flag1
     else: flag1 = not flag1
+    board = [1, '|', 2, '|', 3,
+             4, '|', 5, '|', 6,
+             7, '|', 8, '|', 9]
     return flag1
 
 
-def game():
-    count = 0
+def game(count = 0):
     game_over = False
     flag = True
     while game_over == False:
@@ -85,21 +73,43 @@ def game():
         if flag == True:
             num = "x"
             step = int(input(f"{player1}, ваш ход: "))
-            count+=1
         else:
             num = "o"
             step = int(input(f"{player2}, ваш ход: "))
-            count +=1
         turn(step, num)
-        win = get_result(count)
+        win = get_result()
+        count += 1
+        print(count)
+        if count == 9:
+            win = 3
         if win != 0:
             game_over = True
         else:
             game_over = False
         flag = not (flag)
+        if count == 9:
+            win = 3
     return (win)
 
+def play():
+    while one_more() == True:
+        start_game()
+        winner(game())
+    print("спасибо за игру")
 
+board = [1,'|',2,'|',3,
+        4,'|',5,'|',6,
+        7,'|',8,'|',9]
+
+line_to_victory = [[0, 2, 4],
+             [5, 7, 9],
+             [10, 12, 14],
+             [0, 7, 14],
+             [4, 7, 10],
+             [0, 5, 10],
+             [2, 7, 12],
+             [4, 9, 14]]
+wins = [0, 0]
 player1 = input('Введите имя первого игрока  -  ')
 player2 = input('Введите имя второго игрока  -  ')
 play()
